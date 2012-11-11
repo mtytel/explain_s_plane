7f58887f97f4897b5bbd0705c184a4edb033c16f
Noura <culurienneldoreth@gmail.com>
2012-11-11 14:15:00 -0500

    minor UI improvements

--- init.js
+++ init.js
@@ -1,5 +1,5 @@
 function init() {
-  $('#scale-slider').slider({ max: 100 });
+  $('#scale-slider').slider({ min: 0, max: 100, animate: true, value: 30 });
   make_s_plane_canvas();
   analyserView1 = new AnalyserView("view1");
   initAudio();
